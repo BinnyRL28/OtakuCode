8214c750cdf6001da1ec310176abc8d4d950e6ca
BinnyRL <binny.brl@gmail.com>
2022-01-14 17:18:11 -0500

    Traten de encontrar una condicional

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,49 @@ $(document).ready(function(){
   };
 
   // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  console.log(app);
+    const app = firebase.initializeApp(firebaseConfig);
+  
+    // Inicializar Auth de Firebase
+    const auth = firebase.auth();
+  
+    // Inicializar Auth de Google
+    var provider = new firebase.auth.GoogleAuthProvider();
+  
+    // Inicializar Firestore (Base de datos)
+    const db = firebase.firestore();
+    
+    //A
+    $("#butonRegistrate").click(function (e) {
+      e.preventDefault();
+      // Esto hará que el login desaparezca
+      $("#contenedorLogin").hide();
+      // Esto hara que el formulario de registro aparezca
+      $("#contenedorRegistar").show();
+    })
+
+
+    // Si se completa el formulario de registro y se envia, registra al nuevo usuario y se guarda la sesion
+  $("#butonRegistrar").click(function (e) {
+    e.preventDefault();
+    // Capturamos los datos enviados por el formulario de registro
+    // Campo email
+    var email = $("#registroEmail").val();
+    //Campo Password
+    var password = $("#registroPassword").val();
+    // Metodo de firebase que permite registro de usarios con email
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(userCredential => {
+      // ocultar formulario de registro
+      $("#contenedorRegistar").hide();
+      // limpiar formulario de registro
+      $("#registroUsuario").trigger("reset");
+    })
+    .catch((error) => { // Esto permite capturar el error, se puede trabajar este catch con los codigos de error
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // Muestro en la consola el codigo de error y el mensaje de error
+      console.log(errorCode, errorMessage);
+    });
+})
 });
